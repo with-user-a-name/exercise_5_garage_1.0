27fcf52c463e337840bb5b784ce526402853e9c8
with-user-a-name <micke_999999999@protonmail.com>
2025-11-21 16:11:03 +0100

    Add project files.

---
+++ exercise_5_garage_1.0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace exercise_5_garage_1._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
